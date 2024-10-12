555ad506330f3dbc903e8e052377bc343d5fdedd
wangning <2928824748@qq.com>
2024-10-12 21:53:57 +0800

    worked by cyp

--- springboot/src/main/java/com/example/SpringbootApplication.java
+++ springboot/src/main/java/com/example/SpringbootApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @MapperScan("com.example.mapper")
 public class SpringbootApplication {
-
+    // worked by cyp
     public static void main(String[] args) {
         SpringApplication.run(SpringbootApplication.class, args);
     }
