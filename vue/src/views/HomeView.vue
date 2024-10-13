143844441f8e00c324362be6bfb87308094fb7fa
EpiphanyInPeace <1312398062@qq.com>
2024-10-13 15:07:00 +0800

    add ElTabs in HomeView

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,17 +1,29 @@
 <template>
- <ElContainer style="height: 100%;">
-    <ElHeader style="background-color: chocolate;">
-
-    </ElHeader>
-    <ElMain style="background-color: aliceblue;">
-
-    </ElMain>
- </ElContainer>
+   <ElTabs type="card" style="height: 100%;" v-model="tabName">
+      <ElTabPane label="首页" name="首页">首页</ElTabPane>
+      <ElTabPane label="赛事" name="赛事">赛事</ElTabPane>
+      <ElTabPane label="个人" name="个人">个人</ElTabPane>
+   </ElTabs>
 </template>
 <script setup lang="ts">
+
+import logo from "@/assets/logo.png"
+import type { TabsPaneContext } from "element-plus";
+import {ref} from "vue"
+
+let tabName = ref("首页")
 
 </script>
 
 <style scoped>
+:deep(.el-tabs__header) {
+   background-color: #ff4d00;
+}
 
+:deep(.el-tabs__item) {
+   opacity: 0.8;
+   background-color: aliceblue;
+   width: 100px;
+   font-size: large;
+}
 </style>
