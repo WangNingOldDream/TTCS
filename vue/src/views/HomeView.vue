2892e7d31caf395cfd79aa6c158e79d644fd1178
EpiphanyInPeace <1312398062@qq.com>
2024-10-17 11:58:42 +0800

    improve HomeView geren

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -35,8 +35,7 @@
             infinite-scroll-distance="10" style="height:580px;margin-bottom: 0;overflow: auto;">
             <li v-for="i in compLineNum" :key="i" style="display: flex;justify-content: space-evenly;">
                <ElCard v-for="j in i == compLineNum ? searchedComps.length - 3 * (i - 1) : 3" :key="j" shadow="hover">
-                  <img :src="searchedComps[3 * (i - 1) + j - 1].cover"
-                     style="width: 100%;height: 100%;object-fit: fill;" />
+                  <img :src="searchedComps[3 * (i - 1) + j - 1].cover" style="width: 100%;height: 100%;" />
                   <template #footer>
                      <ElRow justify="center">
                         <ElText style="width: 200px;" truncated size="large">
@@ -105,7 +104,120 @@
                   <ElTableColumn prop="state" label="状态"></ElTableColumn>
                </ElTable>
             </ElTabPane>
-            <ElTabPane label="编辑赛事"></ElTabPane>
+            <ElTabPane label="编辑赛事">
+               <ElRow justify="space-evenly" style="margin-top: 20px;">
+                  <ElCol :span="10">
+                     <ElForm>
+                        <ElFormItem label="赛事名称">
+                           <ElInput v-model="editedComp.name">
+                              <template #suffix>
+                                 <ElButton type="primary">查询</ElButton>
+                              </template>
+                           </ElInput>
+                        </ElFormItem>
+                        <ElFormItem label="举办时间">
+                           <ElDatePicker type="date" v-model="editedComp.date"></ElDatePicker>
+                           <ElTimePicker v-model="editedComp.time"></ElTimePicker>
+                        </ElFormItem>
+                        <ElFormItem label="举办地点">
+                           <ElInput v-model="editedComp.place"></ElInput>
+                        </ElFormItem>
+                        <ElFormItem label="赛制">
+                           <ElSelect style="width: 200px;" v-model="editedComp.format">
+                              <ElOption v-for="item in compFormats" :key="item" :label="item" :value="item">
+                              </ElOption>
+                           </ElSelect>
+                        </ElFormItem>
+                        <ElFormItem label="是否允许观赛">
+                           <ElRadioGroup v-model="editedComp.allowWatch">
+                              <ElRadio value="是">是</ElRadio>
+                              <ElRadio value="否">否</ElRadio>
+                           </ElRadioGroup>
+                        </ElFormItem>
+                        <ElFormItem label="是否允许报名">
+                           <ElRadioGroup v-model="editedComp.allowRegister">
+                              <ElRadio value="是">是</ElRadio>
+                              <ElRadio value="是">否</ElRadio>
+                           </ElRadioGroup>
+                        </ElFormItem>
+                        <ElFormItem label="运动员人数范围">
+                           <ElInputNumber v-model="editedComp.minPlayerNum"></ElInputNumber>
+                           ~
+                           <ElInputNumber v-model="editedComp.maxPlayerNum"></ElInputNumber>
+                        </ElFormItem>
+                        <ElFormItem label="运动员性别限制">
+                           <ElRadioGroup v-model="editedComp.sexLimit">
+                              <ElRadio value="男">男</ElRadio>
+                              <ElRadio value="女">女</ElRadio>
+                              <ElRadio value="不限">不限</ElRadio>
+                           </ElRadioGroup>
+                        </ElFormItem>
+                        <ElFormItem label="运动员年龄范围">
+                           <ElInputNumber v-model="editedComp.minPlayerAge"></ElInputNumber>
+                           ~
+                           <ElInputNumber v-model="editedComp.maxPlayerAge"></ElInputNumber>
+                        </ElFormItem>
+                        <ElFormItem label="运动员运动水平范围">
+                           <ElSelect style="width: 120px;" v-model="editedComp.minTTCLevel">
+                              <ElOption v-for="item in TTLs" :key="item" :label="item" :value="item">
+                              </ElOption>
+                           </ElSelect>
+                           ~
+                           <ElSelect style="width: 120px;" v-model="editedComp.maxTTCLevel">
+                              <ElOption v-for="item in TTLs" :key="item" :label="item" :value="item">
+                              </ElOption>
+                           </ElSelect>
+                        </ElFormItem>
+                     </ElForm>
+                  </ElCol>
+                  <ElCol :span="10">
+                     <ElForm>
+                        <ElFormItem label="赛事封面">
+                           <img :src="editedComp.cover" style="width: 320px;height: 180px;" />
+                        </ElFormItem>
+                        <ElFormItem>
+                           <ElTable :data="participants" style="height: 280px;" highlight-current-row
+                              v-on:current-change="changeRow">
+                              <ElTableColumn type="index" :index="1">
+                              </ElTableColumn>
+                              <ElTableColumn prop="name" label="参与人员"></ElTableColumn>
+                              <ElTableColumn prop="identity" label="参与身份"></ElTableColumn>
+                           </ElTable>
+                        </ElFormItem>
+                        <ElFormItem>
+                           <div style="width: 100%;text-align: center;">
+                              <ElButton v-on:click="addDialog = true">增加</ElButton>
+                              <ElButton v-on:click="deleteParticipants">删除</ElButton>
+                           </div>
+                        </ElFormItem>
+                     </ElForm>
+                  </ElCol>
+               </ElRow>
+               <div style="width: 100%;margin-top: 10px;text-align: center;">
+                  <ElButton>确认</ElButton>
+               </div>
+               <el-dialog v-model="addDialog" title="增加参与人员" width="500">
+                  <el-form>
+                     <el-form-item label="参与人员">
+                        <el-input v-model="addedName" />
+                     </el-form-item>
+                     <el-form-item label="参与身份">
+                        <el-select v-model="addedIdentity">
+                           <el-option label="运动员" value="运动员" />
+                           <el-option label="裁判员" value="裁判员" />
+                        </el-select>
+                     </el-form-item>
+                  </el-form>
+                  <template #footer>
+                     <div class="dialog-footer">
+                        <el-button v-on:click="addDialog = false">取消</el-button>
+                        <el-button type="primary" v-on:click="addParticipant">
+                           确认
+                        </el-button>
+                     </div>
+                  </template>
+               </el-dialog>
+            </ElTabPane>
          </ElTabs>
       </ElTabPane>
    </ElTabs>
@@ -327,6 +439,67 @@ function fixCancel() {
    fixing.value = false;
    userInfo.value = userInfoCopy;
 }
+
+let editedComp = ref({
+   cover: logo,
+   name: "",
+   date: "",
+   time: "",
+   place: "",
+   format: "五局三胜",
+   allowWatch: true,
+   allowRegister: true,
+   maxPlayerNum: 0,
+   minPlayerNum: 0,
+   sexLimit: "不限",
+   maxPlayerAge: 0,
+   minPlayerAge: 0,
+   maxTTCLevel: 0,
+   minTTCLevel: 0,
+})
+
+const compFormats = ["五局三胜", "七局四胜"];
+
+let participants = ref([
+   {
+      name: "1",
+      identity: "运动员"
+   },
+   {
+      name: "2",
+      identity: "运动员"
+   },
+   {
+      name: "3",
+      identity: "裁判员"
+   }
+]);
+
+let addedName = ref("");
+let addedIdentity = ref("")
+
+let addDialog = ref(false)
+
+let currentIndex: number | undefined = undefined;
+
+function addParticipant() {
+   participants.value.push({
+      name: addedName.value,
+      identity: addedIdentity.value
+   });
+   addDialog.value = false;
+}
+
+function changeRow(row: any) {
+   currentIndex = participants.value.indexOf(row);
+}
+
+function deleteParticipants() {
+   if (currentIndex) {
+      participants.value.splice(currentIndex, 1);
+   }
+}
+
 
 </script>
 
